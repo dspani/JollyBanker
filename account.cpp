466bdf1142a69d7da604e9364b716e6f3e5b89f3
Robin Seo <robinseo.seo@gmail.com>
2020-03-02 23:54:59 -0800

    add account.cpp/h

---
+++ account.cpp
@@ -0,0 +1,5 @@
+//
+// Created by changyeon seo on 2020/03/02.
+//
+
+#include "account.h"
---
+++ account.h
@@ -0,0 +1,10 @@
+//
+// Created by changyeon seo on 2020/03/02.
+//
+
+#ifndef JOLLYBANKER_ACCOUNT_H
+#define JOLLYBANKER_ACCOUNT_H
+
+class account {};
+
+#endif // JOLLYBANKER_ACCOUNT_H
