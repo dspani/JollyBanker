7e5a1c85970494b9bd99831be0428b383fdb149f
Robin Seo <robinseo.seo@gmail.com>
2020-03-04 01:55:02 -0800

    add accounttree.cpp add accounttree.h

--- bank.cpp
+++ bank.cpp
@@ -10,6 +10,17 @@ Bank::Bank() = default;
 
 Bank::~Bank() = default;
 
-void Bank::processTransactions(const string& fileName) {}
+void Bank::processTransactions(const string& fileName) {
+    ifstream infile(fileName);
+    string line;
+    vector<string> data;
+    if(infile.is_open()) {
+        while(getline(infile, line)) {
+            // adding in to queue
+            data.push_back(line);
+        }
+        infile.close();
+    }
+}
 
 void Bank::displayAllBankBalances() const {}
