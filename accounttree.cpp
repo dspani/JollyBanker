2e5833c46084bdc44213ff1f058a86054b3e96dc
dspani <rg5vV8RbZvuLQaQPADyc>
2020-03-04 18:41:14 -0800

    ass

--- accounttree.cpp
+++ accounttree.cpp
@@ -21,7 +21,8 @@ bool AccountTree::retrieve(const int& accountNumber, Account*& account) const {
 }
 
 // Display information on all accounts
-void AccountTree::display() const {}
+void AccountTree::display() const {
+}
 
 // delete all information in AccountTree
 void AccountTree::clear() {}
@@ -30,20 +31,20 @@ void AccountTree::clear() {}
 bool AccountTree::isEmpty() const { return true; }
 
 bool AccountTree::openAccount(std::string firstName, std::string lastName, int accNum) {
-
+    return true;
 }
 
 // Deposit x amount to the account number
 bool AccountTree::deposit(int accNum, int amount) {
-
+    return true;
 }
 
 // Withdraw money from the account
 bool AccountTree::withdraw(int accNum, int amount) {
-
+    return true;
 }
 
 // Transfer money from x account to y account
 bool AccountTree::transfer(int toAcc, int fromAcc, int amount) {
-
+    return true;
 }
